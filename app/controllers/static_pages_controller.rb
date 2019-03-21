cc4de2e65e25ec75a36569090a53d5793e3c64d0
Tay LQ <le.quang.tayb@framgia.com>
2019-03-21 12:45:24 +0700

    del book

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,8 +5,8 @@ class StaticPagesController < ApplicationController
     session[:conversations] ||= []
 
     @users = User.all.where.not(id: current_user)
-    @conversations = Conversation.includes(:recipient, :messages)
-                                 .find(session[:conversations])
+    # @conversations = Conversation.includes(:recipient, :messages)
+    #                              .find(session[:conversations])
   end
 
   def help; end
